7cd4741f152553f5fd0e2e793f23d484ed9d03f1
unknown <urchihe@gmail.com>
2018-11-12 11:28:30 +0100

    'server/models/user.js'

--- server/models/user.js
+++ server/models/user.js
@@ -3,12 +3,46 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     username: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      unique: true,
+      validate: {
+      isUnique: function (value, next) {
+      const self = this;
+      User.find({where: {username: value}})
+          .then(function (user) {
+              // reject if a different user wants to use the same email
+       if (user && self.id !== user.id) {
+          return next('Username already in use!');
+          }
+          return next();
+        })
+        .catch(function (err) {
+         return next(err);
+          });
+          }
+        },
     },
     email: {
       type:DataTypes.STRING,
-      allowNull: false
-    },
+      allowNull: false,
+     unique: true,
+     validate: {
+      isUnique: function (value, next) {
+      const self = this;
+      User.find({where: {email: value}})
+          .then(function (user) {
+              // reject if a different user wants to use the same email
+       if (user && self.id !== user.id) {
+          return next('Email already in use!');
+          }
+          return next();
+        })
+        .catch(function (err) {
+         return next(err);
+          });
+          }
+        },
+  },
     password: {
       type:DataTypes.STRING,
       allowNull: false
