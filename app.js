91d1d39d8bdc3ed226d728fc65d12e9d7184549f
unknown <urchihe@gmail.com>
2018-11-12 17:00:10 +0100

    'app.js'

--- app.js
+++ app.js
@@ -14,26 +14,7 @@ const helmet = require('helmet');
 const winstonInstance = require('./server/config/winston');
 const routes = require('./index.routes');
 const APIError = require('./server/controllers/helpers/APIError');
-const { Client } = require('pg');
 
-const client = new Client({
-
-  connectionString: process.env.DATABASE_URL
-
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-
-});
-
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
 
 const app = express();
 
