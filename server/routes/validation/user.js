e1bb8a3b729c0a921c6c4a9aa103940683bc66f5
unknown <urchihe@gmail.com>
2018-11-12 11:30:41 +0100

    'server/routes/validation/user.js'

--- server/routes/validation/user.js
+++ server/routes/validation/user.js
@@ -30,6 +30,7 @@ module.exports = {
     body: {
       username: Joi.string().alphanum().min(3).max(30).required(),
       password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required(),
+      email: Joi.string().regex(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).description('Email is required').required(),
     },
   },
 
