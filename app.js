7028e42b565705e96524ade6cd4589c815448923
unknown <urchihe@gmail.com>
2018-11-12 11:19:34 +0100

    app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use((err, req, res, next) => {
     // validation error contains errors which is an array of error each containing message[]
     const unifiedErrorMessage = err.errors.map(error => error.messages.join('. ')).join(' and ');
     const error = new APIError(unifiedErrorMessage, err.status, true);
-    return next(error);
+    return next([error]);
   } else if (!(err instanceof APIError)) {
     const apiError = new APIError(err.message, err.status, err.isPublic);
     return next(apiError);
