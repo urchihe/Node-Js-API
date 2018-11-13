4565db796548970ed901e1db733aff3e1a9ff10a
unknown <urchihe@gmail.com>
2018-11-13 16:48:05 +0100

    'server/controllers/contacts.js'

--- server/controllers/contacts.js
+++ server/controllers/contacts.js
@@ -84,11 +84,11 @@ function retrieve(req, res) {
     Contact.findById(req.params.contactId)
     .then(contact => {
       if (!contact) {
-        return res.status(400).json({
+        return res.status(204).json({
           message: 'Contact Not Found',
         });
       }else if(contact.userId == user.id){
-        return res.status(400).json({
+        return res.status(204).json({
           message: 'Unauthoried to view this contact',
         });
       }
