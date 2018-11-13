bf55d722d974088780d69bcfd189d035f6d7ad27
unknown <urchihe@gmail.com>
2018-11-13 11:37:53 +0100

    'server\config\winston.js'

--- server/config/winston.js
+++ server/config/winston.js
@@ -7,15 +7,11 @@ const logger = winston.createLogger({
         winston.format.json()
       ),
   transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' })
-  ]
+     new (winston.transports.Console)({
+      json: true,
+      colorize: true
+    })
 });
-if (config.env !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple()
-  }));
-}
 
 module.exports = logger;
 
