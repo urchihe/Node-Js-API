6685e6f299673a83073842bfe65f550511acc6e4
unknown <urchihe@gmail.com>
2018-11-13 11:07:39 +0100

    'app.js'

--- app.js
+++ app.js
@@ -74,9 +74,8 @@ app.use((err, req, res, next) => // eslint-disable-line no-unused-vars
 );
 
 // Require our routes into the application.
-router.route('/')
 
-  .get('*', (req, res) => res.status(200).send({
+router.get('*', (req, res) => res.status(200).send({
   message: 'Contact Api Challenge.',
 }));
 
