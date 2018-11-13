644ba14f0d0e2d556dab8b46affc1b3422feab63
unknown <urchihe@gmail.com>
2018-11-13 12:54:37 +0100

    'server/routes/validation/contact.js'

--- server/routes/validation/contact.js
+++ server/routes/validation/contact.js
@@ -11,7 +11,7 @@ module.exports = {
   create: {
     body: {
       fullname: Joi.string().min(3).max(100).required(),
-      email: Joi.string().regex(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).description('Email is required').required(),
+      email: Joi.string().email().description('Email is required').required(),
       phone: Joi.number().integer().min(10).required(),
       mobile: Joi.number().integer().min(10).required(),
       address:Joi.string().alphanum().min(3).max(100).required(),
@@ -21,7 +21,7 @@ module.exports = {
   update: {
     body: {
       fullname: Joi.string().min(3).max(100).required(),
-      email: Joi.string().regex(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).description('Email is required').required(),
+      email: Joi.string().email().description('Email is required').required(),
       phone: Joi.number().integer().min(10).required(),
       mobile: Joi.number().integer().min(10).required(),
       address:Joi.string().alphanum().min(3).max(100).required(),
